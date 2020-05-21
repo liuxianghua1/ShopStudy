f90102a9cede72dc6972677f489701c48c688348
liuxianghua <3374312139@qq.com>
2020-05-21 10:01:56 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,39 +1,33 @@
 //Page Object
+import { request } from "../../request/index.js";
 Page({
   data: {
-    
+    swiperList: [],
+    cateList: [],
   },
-  //options(Object)
-  onLoad: function(options) {
-    
+  // 页面加载的时候触发的页面
+  onLoad: function (options) {
+    this.getSwiperList();
+    this.getCateList();
   },
-  onReady: function() {
-    
-  },
-  onShow: function() {
-    
-  },
-  onHide: function() {
 
+  getSwiperList() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
+    }).then((res) => {
+      this.setData({
+        swiperList: res.data.message,
+      });
+    });
   },
-  onUnload: function() {
 
+  getCateList() {
+    request({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems",
+    }).then((res) => {
+      this.setData({
+        CateList: res.data.message,
+      });
+    });
   },
-  onPullDownRefresh: function() {
-
-  },
-  onReachBottom: function() {
-
-  },
-  onShareAppMessage: function() {
-
-  },
-  onPageScroll: function() {
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item) {
-
-  }
 });
-  
