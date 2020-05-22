7d5b58bed7f0f9224f119653f533fbe3841631c2
liuxianghua <3374312139@qq.com>
2020-05-22 20:42:26 +0800

    商品列表

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,66 +1,90 @@
+import { request } from "../../request/index.js";
+import Toast from "../../miniprogram_npm/@vant/weapp/toast/toast";
 // pages/goods_list/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: "综合",
+        isActive: true,
+      },
+      {
+        id: 1,
+        value: "销量",
+        isActive: false,
+      },
+      {
+        id: 2,
+        value: "价格",
+        isActive: false,
+      },
+    ],
+    goodsList: [],
   },
+  QueryParams: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize: 10,
+  },
+  totalPages: 0,
+  totalNum: 0,
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(this.QueryParams);
+    this.QueryParams.cid = options.cid;
+    this.getGoodsList();
+  },
+  onReachBottom() {
+    if (this.QueryParams.pagenum > this.totalNum) {
+      Toast("没有下一页了~");
+    } else {
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
+  },
+  async getGoodsList() {
+    Toast.loading({
+      mask: true,
+      message: "加载中...",
+    });
+    const res = await request({ url: "/goods/search", data: this.QueryParams });
+    // 获取条数
+    const total = res.data.message.goods.nv_length;
+    this.totalPages = Math.ceil(total / this.QueryParams.pagesize);
+    this.totalNum = this.totalNum + this.totalPages;
+    this.setData({
+      goodsList: [...this.data.goodsList, ...res.data.message.goods],
+    });
+    Toast.clear();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTabsItemChange(e) {
+    const { index } = e.detail;
+    let { tabs } = this.data;
+    tabs.forEach((v, i) =>
+      i === index ? (v.isActive = true) : (v.isActive = false)
+    );
+    this.setData({
+      tabs,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 下拉刷新时间
+  onPullDownRefresh() {
+    this.setData({
+      goodsList: [],
+    });
+    this.QueryParams.pagenum = 1;
+    this.totalNum = 0;
+    this.getGoodsList();
+    wx.stopPullDownRefresh()
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
