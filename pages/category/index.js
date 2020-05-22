baa5a45c6bdcd1301b3d9eeef3065ef65f4d66dc
liuxianghua <3374312139@qq.com>
2020-05-22 08:43:31 +0800

    分类完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,59 @@
 // pages/category/index.js
+import { request } from "../../request/index.js";
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMentList: [],
+    rightContent: [],
+    currentIndex: 0,
+    scrollTop: 0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  Cates: [],
   onLoad: function (options) {
+    // 先判断本地有无旧数据 没有就直接请求 有旧数据就使用旧数据
+    const Cates = wx.getStorageSync("cates");
+    if (!Cates) {
+      // 不存在
+      this.getCates();
+    } else {
+      // 有数据这时候
+      if (Date.now() - Cates.time > 1000 * 300) {
+        this.getCates();
+      } else {
+        this.Cates = Cates.data;
+        let leftMentList = this.Cates.map((v) => v.cat_name);
+        let rightContent = this.Cates[0].children;
+        this.setData({
+          leftMentList,
+          rightContent,
+        });
+      }
+    }
+  },
+  async getCates() {
+    const res = await request({ url: "/categories" });
+    this.Cates = res.data.message;
+    // 接口存入本地
+    wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
 
+    let leftMentList = this.Cates.map((v) => v.cat_name);
+    let rightContent = this.Cates[0].children;
+    this.setData({
+      leftMentList,
+      rightContent,
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  handleItemTap(e) {
+    let { index } = e.target.dataset;
 
+    let rightContent = this.Cates[index].children;
+    this.setData({
+      currentIndex: index,
+      rightContent,
+      scrollTop: 0,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,16 +4,19 @@ Page({
   data: {
     swiperList: [],
     cateList: [],
+    floorList: [],
   },
   // 页面加载的时候触发的页面
   onLoad: function (options) {
     this.getSwiperList();
     this.getCateList();
+    this.getFloorList();
+
   },
 
   getSwiperList() {
     request({
-      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata",
+      url: "/home/swiperdata",
     }).then((res) => {
       this.setData({
         swiperList: res.data.message,
@@ -23,10 +26,20 @@ Page({
 
   getCateList() {
     request({
-      url: "https://api-hmugo-web.itheima.net/api/public/v1/home/catitems",
+      url: "/home/catitems",
     }).then((res) => {
       this.setData({
         cateList: res.data.message,
+      });
+    });
+  },
+
+  getFloorList() {
+    request({
+      url: "/home/floordata",
+    }).then((res) => {
+      this.setData({
+        floorList: res.data.message,
       });
     });
   },
